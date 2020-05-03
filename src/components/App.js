f91d7f50afd4f0aae967909be1e72d921cb63630
Muata Nkosi <mnkosi@fandm.edu>
2020-05-03 15:03:41 -0400

    Implement Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 export default function App() {
-    const route = window.location.pathname;
-    if(route === '/about') return <AboutPage />;
-    return <HomePage/>
+    function getPage() {
+        const route = window.location.pathname;
+        if(route === '/about') return <AboutPage />;
+        return <HomePage/>
+    }
+
+    return (
+        <div className='container-fluid'>
+            <Header />
+            { getPage() }
+        </div>
+    )
+
+
+
+
 }
