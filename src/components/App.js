dcd6d45495e9d4f87f9388e16734e4ca3157be02
Muata Nkosi <mnkosi@fandm.edu>
2020-05-04 19:52:51 -0400

    Declare routes for react router and include exact as way to specify match for home route

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import HomePage from './HomePage';
+import React from "react";
+import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import { Route } from "react-router-dom";
 
 export default function App() {
-    function getPage() {
-        const route = window.location.pathname;
-        if(route === '/about') return <AboutPage />;
-        if(route === '/courses') return <CoursesPage />
-        return <HomePage/>
-    }
-
-    return (
-        <div className='container-fluid'>
-            <Header />
-            { getPage() }
-        </div>
-    )
-
-
-
-
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/" exact component={HomePage} />
+    </div>
+  );
 }
