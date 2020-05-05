04678e4bce2a939a1c7212315956675f0c49aa9f
Muata Nkosi <mnkosi@fandm.edu>
2020-05-04 20:28:40 -0400

    Using redirect component for handling redirect

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 
 export default function App() {
@@ -14,6 +14,7 @@ export default function App() {
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" exact component={HomePage} />
+        <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
