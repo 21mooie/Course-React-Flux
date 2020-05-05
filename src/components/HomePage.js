3dd23e147973fb6d1acb5c40054bdee37d872e96
Muata Nkosi <mnkosi@fandm.edu>
2020-05-04 20:07:23 -0400

    Use React Router Link Component to route to component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 //React Component should always be named in Pascal Case
 export default function HomePage() {
-    return (
-        <div className='jumbotron'>
-            <h1>Pluralsight Administration</h1>
-            <p>React, Flux, and React Router for ultra-responsive web apps.</p>
-            <a href='/about'>About</a>
-        </div>
-    );
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
+    </div>
+  );
 }
