1f0dfc3febd11b4ead45a6bae79b15c8cf719fda
Muata Nkosi <mnkosi@fandm.edu>
2020-05-05 17:09:54 -0400

    Add react-toastify to toast confirmation message to user after successful submission

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TextInput from "./common/TextInput";
+import PropTypes from "prop-types";
 
 function CourseForm(props) {
   return (
@@ -11,6 +12,7 @@ function CourseForm(props) {
         className="form-control"
         value={props.course.title}
         onChange={props.onChange}
+        error={props.errors.title}
       />
 
       <div className="form-group">
@@ -28,6 +30,9 @@ function CourseForm(props) {
             <option value="2">Scott Allen</option>
           </select>
         </div>
+        {props.errors.authorId && (
+          <div className="alert alert-danger">{props.errors.authorId}</div>
+        )}
       </div>
 
       <TextInput
@@ -36,6 +41,7 @@ function CourseForm(props) {
         name="category"
         className="form-control"
         value={props.course.category}
+        error={props.errors.category}
         onChange={props.onChange}
       />
 
@@ -43,5 +49,12 @@ function CourseForm(props) {
     </form>
   );
 }
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+};
 
 export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -4,6 +4,7 @@ import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -21,10 +22,22 @@ const ManageCoursePage = (props) => {
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
     });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
   }
   return (
     <>
@@ -32,6 +45,7 @@ const ManageCoursePage = (props) => {
       <h2>Manage Course</h2>{" "}
       <CourseForm
         course={course}
+        errors={errors}
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
