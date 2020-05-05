460568ce2c0eec17604fe900be464a406ac87b49
Muata Nkosi <mnkosi@fandm.edu>
2020-05-05 15:59:52 -0400

    Use change handler to map capture react changes and pass it to input

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -11,7 +11,8 @@ function CourseForm(props) {
             type="text"
             name="title"
             className="form-control"
-            value=""
+            value={props.course.title}
+            onChange={props.onTitleChange}
           />
         </div>
       </div>
@@ -22,7 +23,7 @@ function CourseForm(props) {
           <select
             id="author"
             name="authorId"
-            value=""
+            value={props.course.authorId}
             className="form-control"
           >
             <option value="" />
@@ -40,7 +41,7 @@ function CourseForm(props) {
             id="category"
             name="category"
             className="form-control"
-            value=""
+            value={props.course.category}
           />
         </div>
       </div>
