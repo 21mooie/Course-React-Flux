a664c70d8dc1a677157488b55fd5e01cea188638
Muata Nkosi <mnkosi@fandm.edu>
2020-05-04 20:43:45 -0400

    Utilize React Router for passing in route parameters to show information about specific route

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,17 @@ import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 export default function App() {
   return (
     <div className="container-fluid">
       <Header />
       <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/" exact component={HomePage} />
         <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
